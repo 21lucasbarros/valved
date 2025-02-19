b3b1356b378a66dada6f3bb12ad66c7aefb7d267
Lucas Barros <lucasweacked21@gmail.com>
2025-02-19 02:33:41 -0300

    atualizacao: mudanca no index, main e header por completo

--- src/components/Principal/Main.tsx
+++ src/components/Principal/Main.tsx
@@ -18,7 +18,7 @@ export default function Main() {
         </video>
         <section className="principal__conteudo">
           <h1 className="principal__conteudo__titulo">
-            BEM-VINDO AO <span>VALVED</span>
+            BEM-VINDO AO <span>Valved</span>
           </h1>
           <p className="principal__conteudo__paragrafo">
             <span>{text}</span>
@@ -36,6 +36,57 @@ export default function Main() {
           </a>
         </section>
       </main>
+
+      <section className="principais-albuns">
+        <section className="principais-albuns__conteudo">
+          <h2 className="principais-albuns__conteudo__titulo">
+            E O GRAMMY VAI PARA...
+          </h2>
+          <div className="faixa"></div>
+          <section className="principais-albuns__conteudo__lista">
+            <img
+              src="./img/albuns/californication.png"
+              alt="Californication - Red Hot Chili Peppers"
+              className="principais-albuns-card"
+            />
+            <img
+              src="./img/albuns/man-on-the-moon.png"
+              alt="Man on the Moon - Kid Cudi"
+              className="principais-albuns-card"
+            />
+            <img
+              src="./img/albuns/sour.png"
+              alt="Sour - Olivia Rodrigo"
+              className="principais-albuns-card"
+            />
+            <img
+              src="./img/albuns/lover.png"
+              alt="Lover - Taylor Swift"
+              className="principais-albuns-card"
+            />
+            <img
+              src="./img/albuns/dark-side-of-the-moon.png"
+              alt="Dark Side of the-Moon - Pink Floyd"
+              className="principais-albuns-card"
+            />
+            <img
+              src="./img/albuns/blonde.png"
+              alt="Blonde - Frank Ocean"
+              className="principais-albuns-card"
+            />
+            <img
+              src="./img/albuns/dark-side-of-the-moon.png"
+              alt="Happier Than Ever - Billie Eilish"
+              className="principais-albuns-card"
+            />
+            <img
+              src="./img/albuns/timbu.png"
+              alt="MC Timbu"
+              className="principais-albuns-card"
+            />
+          </section>
+        </section>
+      </section>
     </>
   );
 }

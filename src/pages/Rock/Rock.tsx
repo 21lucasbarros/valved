7d478bf7be59da5dce9bd5377c52520950a21e10
Lucas Barros <lucasweacked21@gmail.com>
2025-03-04 21:52:57 -0300

    feat: criacao do componente rock e instalacao do react router.

---
+++ src/pages/Rock/Rock.tsx
@@ -0,0 +1,13 @@
+import "./Rock.css";
+
+export default function Rock() {
+  return (
+    <>
+      <section className="rock">
+        <section className="rock__conteudo">
+          <h1 className="rock__conteudo__titulo">ROCK</h1>
+        </section>
+      </section>
+    </>
+  );
+}
